0d799ff0518e9dbc01ec5748055610fa62820359
Janez Cergolj <me@jcergolj.me.uk>
2024-05-27 14:09:31 +0200

    fix view

--- resources/views/components/notification.blade.php
+++ resources/views/components/notification.blade.php
@@ -20,5 +20,5 @@
                 document.getElementById('flash').style.display = 'none';
             }, {{ InAppNotification::timeout() }});
         </script>
-    @section
+    @endsection
 @endif
